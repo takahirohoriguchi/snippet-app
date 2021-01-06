ef23a21c9fb06a5d4686dbfafeb818d016ae7b0b
takahirohoriguchi <takahiro.horiguchi@di-v.co.jp>
2021-01-06 15:36:06 +0900

    seed

---
+++ db/seeds/20200101_user.rb
@@ -0,0 +1,3 @@
+User.create!([
+  {name: "takahiro", email: "test@test", encrypted_password: "$2a$12$Bqd3fym63SwAsOKT0A5Ww.tspwacF8oUQQjM2S.5FbwnpWIvoFVCy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+])
---
+++ db/seeds/20200102_snippet.rb
@@ -0,0 +1,12 @@
+Snippet.create!([
+  {group_name: "ユーザー管理機能", info: "新規登録/ログイン/ログアウトの機能実装", user_id: 1},
+  {group_name: "商品出品機能", info: "出品", user_id: 1},
+  {group_name: "商品購入機能", info: "商品をクレジットカード決済で購入する機能", user_id: 1},
+  {group_name: "DB設計", info: "データベースの設計", user_id: 1},
+  {group_name: "商品削除機能", info: "商品を削除できるようにする", user_id: 1},
+  {group_name: "商品詳細", info: "商品の詳細を表示する", user_id: 1},
+  {group_name: "issue", info: "issue問題", user_id: 1},
+  {group_name: "コメント", info: "コメントの種類", user_id: 1},
+  {group_name: "商品一覧", info: "商品の一覧を表示する", user_id: 1},
+  {group_name: "商品編集機能", info: "商品を編集する機能", user_id: 1}
+])
